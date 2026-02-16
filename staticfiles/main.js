e7f93182aa7c243e57ed107887362f3796e610cf
Loovon <160861248+Loovon@users.noreply.github.com>
2026-02-16 10:51:34 +0300

    Remove staticfiles from tracking

--- staticfiles/main.js
+++
@@ -1,9 +0,0 @@
-let index = 5;
-const slides = document.querySelector(".slide");
-const totalSlides = slides.children.length;
-
-setInterval(() => {
-    index = (index + 1) % totalSlides;
-    slides.style.transform = `translateX(-${index * 100}%)`;
-    slides.style.transition = "transform 0.5s ease-in-out";
-}, 3000);
